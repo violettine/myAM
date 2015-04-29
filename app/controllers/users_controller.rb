78a4030e0054c4e972578c5dc53a9c4ca39de9a9
violettine <violettine@freenet.de>
2015-04-29 17:04:04 +0200

    prepare for feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  # def show
-  #   @user = User.find(params[:id])
-  #   @microposts = @user.microposts.paginate(page: params[:page])
-  # end
 
-
+  def show
+    #@user = User.find(params[:id])
+    #@microposts = @user.microposts.paginate(page: params[:page])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/help'
   resources :messages, only: [:create, :destroy]
+  get 'users/show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
