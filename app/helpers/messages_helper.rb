a2c2bdd029a5119ba2e061d8b6f261f971470ff1
violettine <violettine@freenet.de>
2015-05-06 15:08:56 +0200

    Issue 1 in add image upload, add view_helper to choose between name and email

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,10 @@
 module MessagesHelper
+
+  def personal_address
+    unless current_user.name.blank?
+      address = current_user.name
+    else 
+      address = current_user.email
+    end
+  end
 end
