2396e9b93b92bbf7a3aecca5ce8a6a3a1ea594de
violettine <violettine@freenet.de>
2015-05-08 14:44:00 +0200

    add to message controller specs

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,6 +35,23 @@ RSpec.describe MessagesController, :type => :controller do
         
         expect(flash[:success]).to match(/^Message deleted!/)
       end
+
+      it "redirects to message path" do
+        sign_in user
+
+        delete :destroy, id: message.id
+
+        expect(response).to redirect_to message_path
+      end
+    end
+    context "when message to destroy is nil" do
+      it "redirects to root_path" do
+        sign_in user
+
+        delete :destroy, id: {message: nil}
+
+        expect(response).to redirect_to root_path
+      end
     end
   end
 end
