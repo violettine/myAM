5abc21341049fd6d72187913de9ee1a2c9a1acc2
violettine <violettine@freenet.de>
2015-05-11 12:09:19 +0200

    changed user_id to integer

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :message do |m|
     m.content {Faker::Lorem.sentence(5)}
-    m.user_id '1'
+    m.user_id 1
   end
 
   factory :invalid_message, parent: :message do |m|
