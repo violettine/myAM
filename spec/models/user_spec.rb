1a9c0a96ec60ade206a7600c3b66b81739b2d9e2
violettine <violettine@freenet.de>
2015-05-11 12:27:23 +0200

    use factoryGirl to create new user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it "renames the user" do
-    user =   User.new
+    user = create(:user)
     user.name = 'new_name'
     
     expect(user.name).to eq 'new_name'
   end
 
   it "not changes email to invalid" do
-    user =   User.new
+    user = create(:user)
     user.email = 'invalid'
     
     expect(user.email).to eq 'invalid'
   end
 
   it "has a valid factory" do
-    expect(FactoryGirl.create(:user)).to be_valid
+    expect(create(:user)).to be_valid
   end
 
   it "is invalid without an email" do
-    expect(FactoryGirl.build(:user, email: nil)).to_not be_valid
+    expect(build(:user, email: nil)).to_not be_valid
   end
 end
