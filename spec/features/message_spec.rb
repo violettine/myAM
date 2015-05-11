ebef720d953697d1a59b2afe76a99c893ffab99a
violettine <violettine@freenet.de>
2015-05-11 10:38:18 +0200

    fix indend

--- spec/features/message_spec.rb
+++ spec/features/message_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'Messages', :type => :feature do 
   describe 'New Message' do
-      let!(:user) {create :user}
+    let!(:user) {create :user}
     it "adds a new message" do
+      
       visit new_user_session_path
-
       fill_in 'user_email', with:user.email
       fill_in 'user_password', with: user.password
       click_button 'Log in'
@@ -16,7 +16,7 @@ describe 'Messages', :type => :feature do
         fill_in 'message_content', with: 'testing content'
         fill_in 'message_author', with: 'test author'
         click_button 'Post' 
-        save_and_open_page
+        #save_and_open_page
       }.to change(Message, :count).by(1)
     end
   end  
